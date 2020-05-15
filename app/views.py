4af075cc241bf2721da40c84d9450742d0bd5a92
Don86 <dh.teng.86@gmail.com>
2020-05-15 10:35:14 +1000

    update

--- app/views.py
+++ app/views.py
@@ -2,6 +2,9 @@
 from app import app
 from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
+import os
+
+from werkzeug.utils import secure_filename
 
 @app.route("/")
 def index():
@@ -32,7 +35,7 @@ def clean_date(dt):
 
 @app.route("/jinja")
 def jinja():
-
+ 
     my_name = "Julian"
     langs = ["Python", "R", "JS", "Ruby"]
     friends_dict = {
@@ -154,3 +157,59 @@ def query():
     if request.args: 
         print(request.args)
     return "Query received", 200
+
+# Example route to demo file uploading
+# This example will upload an image
+
+# set a path to save images to
+# use absolute path
+# If a file of the same name already exists in dir, img.save will override silently. 
+app.config["IMAGE_UPLOADS"] = "/Users/don/Documents/flask_jnash/app/static/img"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
+
+def allowed_image(filename):
+    """File extension sanity check on filename. 
+    Returns True if allowed, False otherwise.
+    """
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".")[1]
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+def allowed_image_filesize(filesize):
+    """Filesize check.
+    """
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        print(request)
+        if request.files: # if there's a file
+            if not allowed_image_filesize(request.cookies["filesize"]):
+                print("File exceeded max size")
+                return redirect(request.url)
+            img = request.files["image"]
+
+            # file input validation checks
+            if img.filename == "":
+                print("Image must have a filename!")
+                return redirect(request.url)
+            if not allowed_image(img.filename):
+                print("That image extension is not allowed")
+                return redirect(request.url)
+            else: # sanitize filename in case it's malicious code
+                filename = secure_filename(img.filename)
+                img.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+            print("Image saved!")
+            return redirect(request.url)
+
+    return render_template("public/upload_image.html")
